bb783d08288995308f6ced3d0a60a76bc7d78c2e
harsha4p28 <harsha4p28@gmail.com>
2025-09-13 05:55:08 +0000

    Initialized

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="AppContainer">
+        <div className="AppMainContainer">
+          <h1>Helllo</h1>
+        </div>
+      </div>
+  );
+}
+
+export default App;
