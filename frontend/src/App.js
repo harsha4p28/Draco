a3b40ed1213d8830b72da570f154f8f492883155
harsha4p28 <harsha4p28@gmail.com>
2025-09-15 10:15:21 +0000

    Dashboard display successful

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+  import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home/Home';
 import Login from './Login/Login';
--- frontend/src/Dashboard/Dashboard.js
+++ frontend/src/Dashboard/Dashboard.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useEffect ,useState} from 'react'
+import config from '../config';
+import './Dashboard.css'
 
 const Dashboard = () => {
+  const [posts, setPosts] = useState ([]);
+  
+  const handleStart = async () =>{
+    try{
+      const res = await fetch(`${config.apiUrl}/posts`,{
+        method:'GET',
+        credentials:'include'
+      });
+      const data = await res.json()
+      setPosts(data)
+    }catch(err){
+      console.log(err);
+    }
+  }
+  useEffect (()=>{
+    handleStart()
+      },[]);
   return (
-    <div>Dashboard</div>
+    <div className="dashboardContainer">
+      <div className="dashboardMainContainer">
+        {posts.map((post) => (
+          <div className="postCard" key={post._id}>
+            {post.image && <img src={post.image} alt={post.title} className="postImage" />}
+            <div className="postContent">
+              <h3 className="postTitle">{post.title}</h3>
+              <p className="postText">{post.content}</p>
+              <div className="postTags">
+                {post.tags.map((tag, index) => (
+                  <span key={index} className="tag">{tag}</span>
+                ))}
+              </div>
+              <p className="postDate">
+                {new Date(post.createdAt).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
